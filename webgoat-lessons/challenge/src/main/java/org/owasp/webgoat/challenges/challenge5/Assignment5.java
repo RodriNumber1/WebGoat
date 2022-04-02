3e5af948e8e819fd7fc1c5bd15e75bede7899ca6
RodriNumber1 <100226279+RodriNumber1@users.noreply.github.com>
2022-04-02 11:28:22 +0200

    Update Assignment5.java

--- webgoat-lessons/challenge/src/main/java/org/owasp/webgoat/challenges/challenge5/Assignment5.java
+++ webgoat-lessons/challenge/src/main/java/org/owasp/webgoat/challenges/challenge5/Assignment5.java
@@ -56,18 +56,29 @@ public class Assignment5 extends AssignmentEndpoint {
         if (!"Larry".equals(username_login)) {
             return failed(this).feedback("user.not.larry").feedbackArgs(username_login).build();
         }
-        
+        PreparedStatement statement = null;
+        ResulSet resultSet = null;
         try (var connection = dataSource.getConnection()) {
             String query = "select password from challenge_users where userid = ? and password = ?";
-            PreparedStatement statement = connection.prepareStatement(query);
+            statement = connection.prepareStatement(query);
             statement.setString(1, username_login);
             statement.setString(2, password_login);
-            ResultSet resultSet = statement.executeQuery();
-            statement.close();
+            resultSet = statement.executeQuery();
             if (resultSet.next()) {
                 return success(this).feedback("challenge.solved").feedbackArgs(Flag.FLAGS.get(5)).build();
             } else {
                 return failed(this).feedback("challenge.close").build();
+            }
+        } finally {
+            
+            try {
+                if (resultSet != null){
+                    resultSet.close();
+                }
+            } finally {
+                if (statement != null){
+                    statement.close();
+                }
             }
         }
     }
