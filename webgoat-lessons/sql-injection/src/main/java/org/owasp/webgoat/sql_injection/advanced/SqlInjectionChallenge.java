b1675fe4257b4c04883565ef38768197e6d92849
Oscar del Rio <91894632+Oscarmanz@users.noreply.github.com>
2022-04-02 12:38:38 +0200

    Update SqlInjectionChallenge.java

--- webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/sql_injection/advanced/SqlInjectionChallenge.java
+++ webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/sql_injection/advanced/SqlInjectionChallenge.java
@@ -59,10 +59,14 @@ public class SqlInjectionChallenge extends AssignmentEndpoint {
         if (attackResult == null) {
 
 
+
+            Statement statement = null;
+            ResultSet resultSet = null;
             try (Connection connection = dataSource.getConnection()) {
-                String checkUserQuery = "select userid from sql_challenge_users where userid = '" + username_reg + "'";
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery(checkUserQuery);
+                String checkUserQuery = "select userid from sql_challenge_users where userid = ?";
+                statement = connection.createStatement();
+                statement.setString(1, username_reg);
+                resultSet = statement.executeQuery(checkUserQuery);
 
                 if (resultSet.next()) {
                     if (username_reg.contains("tom'")) {
@@ -80,6 +84,16 @@ public class SqlInjectionChallenge extends AssignmentEndpoint {
                 }
             } catch (SQLException e) {
                 attackResult = failed(this).output("Something went wrong").build();
+            } finally {
+                try {
+                    if(resultSet != null){
+                        resultSet.close();
+                    }
+                } finally {
+                    if (statement != null){
+                        statement.close();
+                    }
+                }
             }
         }
         return attackResult;
